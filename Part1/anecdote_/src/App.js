966b903057ac4daeab190823dec855f83f6e6806
Keenan Lee-Peters <96672628+klp1130@users.noreply.github.com>
2022-04-11 06:47:24 -0600

    Add files via upload

---
+++ Part1/anecdote_/src/App.js
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+    </button>
+
+)
+
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [likes, setLiked] = useState(Array(anecdotes.length).fill(0))
+
+  const randomQuotes = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+
+  const registerLike = () => {
+    const newLikes = 
+    [...likes]
+    newLikes[selected] += 1
+    setLiked(newLikes)
+  }
+
+
+  const findMostLiked = () => likes.indexOf(Math.max(...likes));
+
+
+  return (
+    <div>
+    
+      <h2>Anecdote of the day</h2>
+      {anecdotes[selected]}
+      <br></br>
+      <p>has {likes[selected]}</p>
+      <Button handleClick={randomQuotes} text='next dev anecdote' />
+      <Button handleClick={registerLike} text='like' />
+      <h2>Anecdote with most likes</h2>
+      <p>{anecdotes[findMostLiked()]}</p>
+      <p>has {likes[findMostLiked()]} votes </p>
+    </div>
+  )
+}
+
+export default App
